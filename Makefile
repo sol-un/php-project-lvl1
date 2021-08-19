07f85365c7ab964938bc671cd7297320f5641f7a
Andrei Soloviov <solovyev833@gmail.com>
2021-08-19 21:12:36 +0300

    Restore brain-games for Hexlet check

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 install:
 	composer install
+brain-games:
+	./bin/brain-games
 brain-even:
 	./bin/brain-even
 brain-calc:
---
+++ src/brain-games.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Brain\Games\Brain\Games;
+
+function greet()
+{
+  print_r('Welcome to the Brain Games!');
+}
